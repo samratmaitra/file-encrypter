ea1a546e0699101a6840ba4819f050d55bcfeece
Samrat Maitra <samrat.maitra@ymail.com>
2023-03-19 20:44:45 +0530

    Working app

--- src/app.py
+++ src/app.py
@@ -4,12 +4,12 @@ from file_processor import FileProcessor
 def load_page() -> list:
     st.header("File Encryption / Decryption Util")
     with st.form("frm_main", clear_on_submit=True):
-        files = st.file_uploader("Choose Files:", accept_multiple_files=True)
+        file = st.file_uploader("Choose File:", accept_multiple_files=False)
         secret_key = st.text_input("Enter secret key:", type="password")
         btn_submit = st.form_submit_button("Submit")
         if btn_submit:
-            if files and secret_key:
-                st.session_state['files'] = files
+            if file and secret_key:
+                st.session_state['file'] = file
                 st.session_state['secret_key'] = secret_key
                 return btn_submit
             else:
@@ -18,8 +18,5 @@ def load_page() -> list:
 if __name__ == "__main__":
     if load_page():
         fp = FileProcessor(st.session_state['secret_key'])
-        st.write(st.session_state['files'])
-        st.write(st.session_state['secret_key'])
-        for file in st.session_state['files']:
-            bytes_data = file.getvalue()
-            st.write(bytes_data)
+        ip_data = st.session_state['file'].getvalue()
+        fp.process(ip_data)
--- src/file_processor.py
+++ src/file_processor.py
@@ -1,43 +1,24 @@
-import os
-import time
-import concurrent.futures
+import streamlit as st
 from itertools import cycle
 
 class FileProcessor:
 
     def __init__(self, key:str) -> None:
         self.__key = key.encode('ascii')
-
-    @staticmethod
-    def file_read(src_file_path:str) -> bytes:
-        with open(src_file_path, 'rb') as f:
-            content_chars = f.read()
-        return content_chars
     
     @staticmethod
-    def file_write(data:bytes, tgt_file_path:str) -> None:
-        try:
-            os.mkdir(os.path.dirname(tgt_file_path))
-        except:
-            pass
-        with open(tgt_file_path, 'wb') as f:
-            f.write(data)
+    def file_download(data:bytes) -> None:
+        st.caption(f"{st.session_state['file'].name} is processed and is ready for download.")
+        st.download_button(
+            label="Download",
+            data=data,
+            file_name=f"Processed_{st.session_state['file'].name}",
+            mime=st.session_state['file'].type)
 
-    def process(self, src_file_path:str, tgt_file_path:str) -> None:
-        print(f"Processing file - {src_file_path}...")
-        data = self.file_read(src_file_path)
-        processed_data = bytes(a ^ b for a, b in zip(data, cycle(self.__key)))
-        self.file_write(processed_data, tgt_file_path)
-        print(f"Processing file - {src_file_path} to {tgt_file_path} is done.")
+    def process(self, data:bytes) -> bytes:
+        with st.spinner(f"Processing file {st.session_state['file'].name}..."):
+            processed_data = bytes(x ^ y for x, y in zip(data, cycle(self.__key)))
+            self.file_download(processed_data)
 
 if __name__ == "__main__":
-    start = time.time()
-    fp = FileProcessor("my secret key")
-    video_path = 'G:\Lectures\PostgreSQL\S1\Processed'
-    videos = os.listdir(video_path)
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        procs = [executor.submit(fp.process,os.path.join(video_path, video),
-                                                os.path.join(video_path, "Processed", video)) 
-                                                for video in videos]
-    finish = time.time()
-    print(f"Total time taken: {finish-start} second(s).")
+    pass
